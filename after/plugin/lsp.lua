0e81573eab018ae7f0e3a0220b3f31e11dd36cab
Jesus Lizama <jesus@studio3marketing.com>
2023-11-30 23:06:59 -0600

    chore: indentation

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,10 +8,10 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-		ensure_installed = {"tsserver"},
-		handlers = {
-				lsp_zero.default_setup,
-		},
+    ensure_installed = {"tsserver"},
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
 
 require('lspconfig').tsserver.setup({})
