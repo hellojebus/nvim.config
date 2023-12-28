a264a1779d91fac353058909da7fcf6ccf8a94eb
Jesus Lizama <jesus@studio3marketing.com>
2023-12-28 09:42:01 -0600

    chore: add hunk maps

--- lua/zeus/remap.lua
+++ lua/zeus/remap.lua
@@ -7,5 +7,11 @@ Map("v", "K", ":m '<-2<CR>gv=gv")
 --replace current buffer
 Map("n", "<leader>r", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
 
+
 -- Files
 Map("n", "<leader>1", ":Ex<CR>")
+
+-- Git
+Map("n", "<leader>hd", ":Gitsigns diffthis<CR>")
+Map("n", "<leader>hp", ":Gitsigns preview_hunk<CR>")
+Map("n", "<leader>hb", ":Gitsigns toggle_current_line_blame<CR>")
