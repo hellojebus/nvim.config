2b08de274cb21e03183514cefe9c92ac91095540
Jesus Lizama <jesus@studio3marketing.com>
2024-01-11 09:27:15 -0600

    chore: add neotree

--- lua/zeus/packer.lua
+++ lua/zeus/packer.lua
@@ -33,6 +33,15 @@ require('pckr').add {
     run = ':TSUpdate'
   },
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  },
+  {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     requires = {
