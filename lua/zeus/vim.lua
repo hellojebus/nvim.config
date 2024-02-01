a65df0b539116ddb2626d1442f4df92fa0b99983
Jesus Lizama <jesus@studio3marketing.com>
2024-02-01 09:34:57 -0600

    chore: code cleanup

--- lua/zeus/vim.lua
+++ lua/zeus/vim.lua
@@ -1,1 +1,13 @@
-vim.o.tabstop = 2
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+vim.opt.swapfile = false
+
+vim.opt.cursorline = true
