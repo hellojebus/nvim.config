11472e944c8287a969817463e953ce11b02c8176
Jesus Lizama <jesus@studio3marketing.com>
2024-02-12 10:44:01 -0600

    chore: enable lsp_zero formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,7 @@ lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
+  lsp_zero.buffer_autoformat()
 end)
 
 require('mason').setup({})
